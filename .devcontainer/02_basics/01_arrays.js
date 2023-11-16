ddfb1bcc99e53b574be6c18bac51d0a9ffc8d915
pashaamir7 <128328675+pashaamir7@users.noreply.github.com>
2023-11-16 11:28:53 +0000

    container config done

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -11,6 +11,16 @@
 // const newArray=myArr.join();
 // console.log(newArray);
 
-const myHeroes=["Batman","Hulk", "Thor", "Groot"]
+const myHeroes=["Batman","Hulk", "Thor", "Groot"];
+
+const myVil=["Joker","Thanos", "Electra", "Lokiji"];
+
+// myHeroes.push(myVil);
+
+// const allHeroes = myHeroes.concat(myVil);
+
+const allHeroes=[...myHeroes,...myVil];
+
+console.log(allHeroes);
 
 
